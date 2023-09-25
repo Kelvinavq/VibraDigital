780a7a1aeb9de085da49e0dadb338674888d292b
Kelvin <kvalera200244@gmail.com>
2023-09-25 15:41:12 -0400

    update

--- js/menu.js
+++ js/menu.js
@@ -16,6 +16,29 @@ const li = document.querySelectorAll(".li");
 
 li.forEach((element) => {
   element.addEventListener("click", () => {
+    document.querySelectorAll(".li").forEach(item => item.classList.remove("active"));
     document.querySelector("nav").classList.remove("active");
+    element.classList.add("active")
   });
 });
+
+$location = window.location.hash;
+
+switch ($location) {
+  case "#inicio":
+    document.querySelector(".menu-inicio").classList.add("active");
+    break;
+  case "#ventajas":
+    document.querySelector(".menu-ventajas").classList.add("active");
+    break;
+  case "#procesos":
+    document.querySelector(".menu-procesos").classList.add("active");
+    break;
+  case "#portafolio":
+    document.querySelector(".menu-portafolio").classList.add("active");
+    break;
+
+  default:
+    document.querySelector(".menu-inicio").classList.add("active");
+    break;
+}
